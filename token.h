ae9cdb84b73fd1d1c0bdb8929abe3e531b6c826f
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-10-04 22:32:45 +0300

    Add files via upload

---
+++ token.h
@@ -0,0 +1,26 @@
+//
+// Created by MacOS on 2020-09-01.
+//
+
+#pragma once
+
+#include <sstream>
+#include <vector>
+using namespace std;
+
+enum class TokenType {
+    DATE,
+    EVENT,
+    COLUMN,
+    LOGICAL_OP,
+    COMPARE_OP,
+    PAREN_LEFT,
+    PAREN_RIGHT,
+};
+
+struct Token {
+    const string value;
+    const TokenType type;
+};
+
+vector<Token> Tokenize(istream& cl);
